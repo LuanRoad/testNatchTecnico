cdf2ae5235c218e555e59b3eed0b08913807252e
Luis Angel Rodríguez Angeles <luisalt_nes64@hotmail.com>
2022-04-22 01:10:43 -0500

    Initial Commit

---
+++ testNatch2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testNatch2022
+//
+//  Created by Luan Road on 22/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testNatch2022UITests/testNatch2022UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  testNatch2022UITestsLaunchTests.swift
+//  testNatch2022UITests
+//
+//  Created by Luan Road on 22/04/22.
+//
+
+import XCTest
+
+class testNatch2022UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
